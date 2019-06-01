e351b8fe0f1906ea17a05dc59c727011e9b7615b
PiroHiroPiro <hirohiro11071107@gmail.com>
2019-06-02 01:04:06 +0900

    file select form

--- src/vuesplash/resources/js/components/Navbar.vue
+++ src/vuesplash/resources/js/components/Navbar.vue
@@ -5,10 +5,11 @@
         </RouterLink>
         <div class="navbar__menu">
             <div v-if="isLogin" class="navbar__item">
-                <button class="button">
+                <button class="button" @click="showForm = ! showForm">
                     <i class="icon ion-md-add"></i>
                     Submit a photo
                 </button>
+
             </div>
             <span v-if="isLogin" class="navbar__item">
                 {{ username }}
@@ -19,11 +20,22 @@
                 </RouterLink>
             </div>
         </div>
+        <PhotoForm v-model="showForm" />
     </nav>
 </template>
 
 <script>
+    import PhotoForm from './PhotoForm.vue'
+
     export default {
+        components: {
+            PhotoForm
+        },
+        data () {
+            return {
+                showForm: false
+            }
+        },
         computed: {
             isLogin () {
                 return this.$store.getters['auth/check']
