b55cfce162d4ea15de2d76972b5ca15fac22a4e1
PiroHiroPiro <hirohiro11071107@gmail.com>
2019-06-01 21:15:12 +0900

    system error

--- src/vuesplash/resources/js/store/auth.js
+++ src/vuesplash/resources/js/store/auth.js
@@ -1,5 +1,8 @@
+import { OK } from '../util'
+
 const state = {
-    user: null
+    user: null,
+    apiStatus: null
 };
 
 const getters = {
@@ -16,20 +19,32 @@ const mutations = {
 const actions = {
     async register (context, data) {
         const response = await axios.post('/api/register', data);
-        context.commit('setUser', response.data);
+        context.commit('setUser', response.data)
     },
     async login (context, data) {
-        const response = await axios.post('/api/login', data);
-        context.commit('setUser', response.data);
+        // 最初はnull
+        context.commit('setApiStatus', null);
+        const response = await axios.post('/api/login', data).catch(err => err.response || err);
+
+        if (response.status === OK) {
+            // 成功したらtrue
+            context.commit('setApiStatus', true);
+            context.commit('setUser', response.data);
+            return false
+        }
+
+        // 失敗だったらfalse
+        context.commit('setApiStatus', false);
+        context.commit('error/setCode', response.status, { root: true })
     },
     async logout (context) {
         const response = await axios.post('/api/logout');
-        context.commit('setUser', null);
+        context.commit('setUser', null)
     },
     async currentUser (context) {
         const response = await axios.get('/api/user');
         const user = response.data || null;
-        context.commit('setUser', user);
+        context.commit('setUser', user)
     }
 };
 
